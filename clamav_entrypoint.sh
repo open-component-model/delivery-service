2651614ccde922afb276165abff31d17e761c3e5
zkdev <dev@zeekay.dev>
2024-04-29 12:24:45 +0200

    Add meaningful logs during clamav worker bootstrap

--- clamav_entrypoint.sh
+++ clamav_entrypoint.sh
@@ -7,12 +7,13 @@ chown -R clamav /run/clamav
 
 # start up freshclam to pull the definitions. In our deployment, these will be pulled
 # from a cluster-local mirror
+echo "Starting Freshclam daemon ..."
 freshclam --daemon
 # we need to sleep a while here since we start freshclam as a daemon in the background.
 # Give it time to download the definitions.
 echo "Sleeping for 45 seconds to give freshclam time to download the virus definitions ..."
 sleep 45
-echo "Done"
+echo "Starting ClamD ..."
 clamd
 
 # watch logfile
