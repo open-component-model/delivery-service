cf8ea385c6b6940c0bc0a2ad95313228c8f5d515
Philipp Heil (zkdev) <dev@zeekay.dev>
2024-11-12 13:54:03 +0100

    fix: Properly filter for overdue findings when jumping to delivery-dashboard

--- issue_replicator/github.py
+++ issue_replicator/github.py
@@ -961,6 +961,8 @@ def _create_or_update_issue(
         extra=extra_title,
     )
 
+    is_overdue = latest_processing_date < datetime.date.today()
+
     template_variables = _template_vars(
         cfg_name=cfg_name,
         issue_replicator_config=issue_replicator_config,
@@ -970,7 +972,7 @@ def _create_or_update_issue(
         findings=findings,
         artefact_ids_without_scan=artefact_ids_without_scan,
         latest_processing_date=latest_processing_date,
-        sprint_name=sprint_name,
+        sprint_name='Overdue' if is_overdue else sprint_name,
     )
 
     for issue_template_cfg in issue_replicator_config.github_issue_template_cfgs:
@@ -981,7 +983,7 @@ def _create_or_update_issue(
 
     body = issue_template_cfg.body.format(**template_variables)
 
-    if latest_processing_date < datetime.date.today():
+    if is_overdue:
         labels.add(gci._label_overdue)
 
     if not is_scanned:
