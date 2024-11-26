80f5ce50fb5598b2b34c22c498a591b8784ccbb6
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2024-11-26 08:21:19 +0100

    Consider all sprints for creation of GitHub milestones
    If the processing date of a finding lies too much in the past and we
    don't consider enough sprints, it may happen that an issue is not
    assigned to any sprint because no related milestone is kept open. By
    considering all available sprints, it will always be assigned to a
    sprint (assuming there is at least one not-closed milestone).

--- issue_replicator/github.py
+++ issue_replicator/github.py
@@ -246,7 +246,6 @@ def _issue_milestone(
     sprints = gcmi.target_sprints(
         delivery_svc_client=delivery_client,
         latest_processing_date=latest_processing_date,
-        sprints_count=12,
     )
 
     return gcmi.find_or_create_sprint_milestone(
