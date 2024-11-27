1b62776fc5197dc600bdadfddc7e2858d61a186f
TuanAnh17N <Tuan-Anh_Nguyen@gmx.de>
2024-11-27 16:27:26 +0100

    Extend unit-testing for sast

--- test/test_rescoring.py
+++ test/test_rescoring.py
@@ -1,7 +1,10 @@
 import pytest
 import dacite
+import datetime
 
+import dso.model
 import rescore.model
+import rescore.utility
 
 
 @pytest.fixture
@@ -126,3 +129,188 @@ def test_deserialise_with_extra_attributes(
 
     assert isinstance(cve_rescoring_rule_sets[0], rescore.model.CveRescoringRuleSet)
     assert len(cve_rescoring_rule_sets[0].rules) == 3
+
+
+@pytest.fixture
+def sast_rescoring_rules_raw() -> dict:
+    return {
+        'defaultRuleSetNames': [
+            {
+                'name': 'my-sast-rescoring',
+                'type': 'sast',
+            }
+        ],
+        'rescoringRuleSets': [
+            {
+                'name': 'my-sast-rescoring',
+                'type': 'sast',
+                'rules': [
+                    {
+                        'name': 'public-no-linting',
+                        'component_context': 'public',
+                        'sast_status': 'no-linting',
+                        'rescore': 'blocker'
+                    },
+                    {
+                        'name': 'internal-local-linting',
+                        'component_context': 'sap-internal',
+                        'sast_status': 'local-linting',
+                        'rescore': 'to-none'
+                    },
+                    {
+                        'name': 'internal-central-linting',
+                        'component_context': 'sap-internal',
+                        'sast_status': 'central-linting',
+                        'rescore': 'to-none'
+                    },
+                ],
+            }
+        ]
+    }
+
+
+def test_deserialise_sast_rescoring_rule_sets(
+    sast_rescoring_rules_raw: dict,
+):
+    sast_rescoring_rule_sets = tuple(
+        rescore.model.SastRescoringRuleSet(
+            name=sast_rule_set_raw['name'],
+            description=sast_rule_set_raw.get('description'),
+            rules=list(
+                rescore.model.sast_rescoring_rules(sast_rule_set_raw['rules'])
+            )
+        )
+        for sast_rule_set_raw in sast_rescoring_rules_raw['rescoringRuleSets']
+    )
+
+    assert isinstance(sast_rescoring_rule_sets[0], rescore.model.SastRescoringRuleSet)
+
+    ruleset = sast_rescoring_rule_sets[0]
+    assert len(ruleset.rules) == 3
+
+    rule1, rule2, rule3 = ruleset.rules
+    assert rule1.rescore is rescore.model.Rescore.BLOCKER
+    assert rule2.rescore is rescore.model.Rescore.TO_NONE
+    assert rule3.rescore is rescore.model.Rescore.TO_NONE
+
+
+def test_deserialise_sast_rescoring_rule_sets_default_rule_set_names(
+    sast_rescoring_rules_raw: dict,
+):
+    default_rule_sets = [
+        dacite.from_dict(
+            data_class=rescore.model.DefaultRuleSet,
+            data=item,
+            config=dacite.Config(
+                cast=[rescore.model.RuleSetType],
+            )
+        )
+        for item in sast_rescoring_rules_raw['defaultRuleSetNames']
+    ]
+
+    sast_rescoring_rule_sets = tuple(
+        rescore.model.SastRescoringRuleSet(
+            name=sast_rule_set_raw['name'],
+            description=sast_rule_set_raw.get('description'),
+            rules=list(
+                rescore.model.sast_rescoring_rules(sast_rule_set_raw['rules'])
+            )
+        )
+        for sast_rule_set_raw in sast_rescoring_rules_raw['rescoringRuleSets']
+    )
+
+    default_rule_set = rescore.model.find_default_rule_set_for_type_and_name(
+        default_rule_set=rescore.model.find_default_rule_set_for_type(
+            default_rule_sets=default_rule_sets,
+            rule_set_type=rescore.model.RuleSetType.SAST,
+        ),
+        rule_sets=sast_rescoring_rule_sets,
+    )
+
+    assert default_rule_set is not None
+    assert default_rule_set.name == "my-sast-rescoring"
+    assert default_rule_set.type == rescore.model.RuleSetType.SAST
+
+
+@pytest.fixture
+def sast_finding_public():
+    return dso.model.ArtefactMetadata(
+        artefact=dso.model.ComponentArtefactId(
+            component_name='public-component',
+            component_version='1.0.0',
+            artefact=dso.model.LocalArtefactId(
+                artefact_name=None,
+                artefact_type=None,
+            )
+        ),
+        meta=dso.model.Metadata(
+            datasource=dso.model.Datasource.CM06,
+            type=dso.model.Datatype.SAST_FINDING,
+            creation_date=datetime.datetime.now(),
+            last_update=datetime.datetime.now(),
+        ),
+        data=dso.model.SastFinding(
+            component_context=rescore.model.ComponentContext.SAP_INTERNAL.value,
+            sast_status=rescore.model.SastStatus.CENTRAL_LINTING.value,
+            severity=rescore.model.Rescore.BLOCKER.value,
+        )
+    )
+
+
+@pytest.fixture
+def sast_finding_internal():
+    return dso.model.ArtefactMetadata(
+        artefact=dso.model.ComponentArtefactId(
+            component_name='internal-component',
+            component_version='1.0.0',
+            artefact=dso.model.LocalArtefactId(
+                artefact_name=None,
+                artefact_type=None,
+            )
+        ),
+        meta=dso.model.Metadata(
+            datasource=dso.model.Datasource.CM06,
+            type=dso.model.Datatype.SAST_FINDING,
+            creation_date=datetime.datetime.now(),
+            last_update=datetime.datetime.now(),
+        ),
+        data=dso.model.SastFinding(
+            component_context=rescore.model.ComponentContext.SAP_INTERNAL.value,
+            sast_status=rescore.model.SastStatus.CENTRAL_LINTING.value,
+            severity=rescore.model.Rescore.BLOCKER.value,
+        )
+    )
+
+
+def test_generate_sast_rescorings(
+    sast_rescoring_rules_raw: dict,
+    sast_finding_public: dso.model.ArtefactMetadata,
+    sast_finding_internal: dso.model.ArtefactMetadata,
+):
+    sast_rescoring_rule_sets = tuple(
+        rescore.model.SastRescoringRuleSet(
+            name=sast_rule_set_raw['name'],
+            description=sast_rule_set_raw.get('description'),
+            rules=list(
+                rescore.model.sast_rescoring_rules(sast_rule_set_raw['rules'])
+            )
+        )
+        for sast_rule_set_raw in sast_rescoring_rules_raw['rescoringRuleSets']
+    )
+    sast_rescoring_ruleset = sast_rescoring_rule_sets[0]
+
+    rescored_metadata = rescore.utility.generate_sast_rescorings(
+        findings=[sast_finding_public, sast_finding_internal],
+        sast_rescoring_ruleset=sast_rescoring_ruleset,
+        user=dso.model.User(
+            username="test_user",
+        ),
+    )
+
+    # 2 rescored & 2 original Findings
+    assert len(rescored_metadata) == 4
+
+    original_public, rescored_public = rescored_metadata[:2]
+    assert original_public.data.sast_status == rescore.model.SastStatus.CENTRAL_LINTING.value
+    assert rescored_public.data.severity == 'to-none'
+    assert rescored_public.data.matching_rules == ['internal-central-linting']
