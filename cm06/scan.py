2bf01206efdc91f3eac1f410696d9523537895a8
TuanAnh17N <Tuan-Anh_Nguyen@gmx.de>
2024-12-10 19:52:50 +0100

    Refactor SAST worker: modularize functions, update config, use new rules

--- cm06/scan.py
+++ cm06/scan.py
@@ -5,8 +5,8 @@ import datetime
 import enum
 import logging
 import os
+
 import semver
-import sys
 
 import ci.log
 import cnudie.iter
@@ -14,11 +14,11 @@ import cnudie.retrieve
 import delivery.client
 import dso.labels
 import dso.model
+import github.compliance.model
 import ocm
 
 import config
 import ctx_util
-import github.compliance.model
 import k8s.util
 import k8s.model
 import k8s.logging
@@ -38,100 +38,115 @@ class AnalysisLabel(enum.StrEnum):
     SAST = 'sast'
 
 
-def sast_status(
+def has_local_linter(
     component: ocm.Component
-) -> rescore.model.SastStatus:
-    local_lint = False
-    central_lint = True # default case if no label and no sast evidence artefact
+) -> bool:
+    for resource in component.resources:
+        if not (label := resource.find_label(name=dso.labels.PurposeLabel.name)):
+            continue
+
+        label_content = dso.labels.deserialise_label(label)
+        if AnalysisLabel.SAST.value in label_content.value:
+            return True
+
+    return False
+
+
+def is_central_linter_ignored(
+    component: ocm.Component
+) -> bool:
+    if label := component.find_label(name=dso.labels.SourceScanLabel.name):
+        label_content = dso.labels.deserialise_label(label)
+        if label_content.value.policy is dso.labels.ScanPolicy.SKIP:
+            return True
+
+    if not component.sources:
+        return True
 
     for source in component.sources:
-        if label := source.find_label(name=dso.labels.SourceScanLabel.name):
-            label_content = dso.labels.deserialise_label(label)
-            if label_content.value.policy is dso.labels.ScanPolicy.SKIP:
-                central_lint = False
+        if not (label := source.find_label(name=dso.labels.SourceScanLabel.name)):
+            continue
 
-    for resource in component.resources:
-        if label := resource.find_label(name=dso.labels.ResourceScanLabel.name):
-            label_content = dso.labels.deserialise_label(label)
-            if AnalysisLabel.SAST.value in label_content.value:
-                local_lint = True
+        label_content = dso.labels.deserialise_label(label)
+        if label_content.value.policy is dso.labels.ScanPolicy.SKIP:
+            return True
 
-    if local_lint:
-        return rescore.model.SastStatus.LOCAL_LINTING
-    elif central_lint:
-        return rescore.model.SastStatus.CENTRAL_LINTING
-    else:
-        return rescore.model.SastStatus.NO_LINTING
+    return False
 
 
-def determine_component_context(
-    component: ocm.Component,
-    cfg: config.CM06Config
-) -> str:
-    for mapping in cfg.component_context_mapping:
-        for prefix in mapping.ocm_repo_prefixes:
-            if component.name.startswith(prefix):
-                return mapping.context_name
-
-    return dso.model.ComponentContext.INTERNAL.value
-
-
-def deserialise_cm06_configuration(
-    cfg_name: str,
+def deserialise_sast_configuration(
+    scan_cfg_name: str,
     namespace: str,
     kubernetes_api: k8s.util.KubernetesApi,
-) -> config.CM06Config:
+) -> config.SASTConfig:
     scan_cfg_crd = kubernetes_api.custom_kubernetes_api.get_namespaced_custom_object(
         group=k8s.model.ScanConfigurationCrd.DOMAIN,
         version=k8s.model.ScanConfigurationCrd.VERSION,
         plural=k8s.model.ScanConfigurationCrd.PLURAL_NAME,
         namespace=namespace,
-        name=cfg_name,
+        name=scan_cfg_name,
     )
     if scan_cfg_crd and (spec := scan_cfg_crd.get('spec')):
-        cm06_config = config.deserialise_cm06_config(spec_config=spec)
+        sast_config = config.deserialise_sast_config(spec_config=spec)
     else:
-        cm06_config = None
+        sast_config = None
 
-    if not cm06_config:
-        logger.warning(
-            f'No CM06 configuration found for scan configuration {cfg_name} in namespace {namespace}'
-        )
-        sys.exit(0)
-
-    return cm06_config
-
-
-def get_landscape_versions(
-    cm06_config,
-    delivery_client
-) -> list[str]:
-    # If both component_version and audit_timerange_months are provided, raise an error
-    if cm06_config.component_version and cm06_config.audit_timerange_months:
+    if not sast_config:
         raise ValueError(
-            'Cannot use both component_version and audit_timerange_months simultaneously.'
+            f'No SAST configuration found for scan config {scan_cfg_name} in namespace {namespace}'
         )
 
-    # If a specific component version is provided, return it
-    if cm06_config.component_version:
-        return [cm06_config.component_version]
+    return sast_config
+
+
+def get_component_versions(
+    sast_config: config.SASTConfig,
+    delivery_client: delivery.client.DeliveryServiceClient,
+) -> list[str]:
+    if sast_config.component_version:
+        return [sast_config.component_version]
 
     # Default start_date to today
-    start_date = cm06_config.audit_start_date or datetime.date.today()
-
-    # Calculate end_date based on audit_timerange_months
-    if cm06_config.audit_timerange_months:
-        end_date = start_date - datetime.timedelta(
-            days=cm06_config.audit_timerange_months * 30
-        )
-    else:
-        # Default end_date to start_date if no range is given
-        end_date = start_date
+    start_date = datetime.date.today()
+    # Calculate end_date based on audit_timerange_days
+    end_date = start_date - datetime.timedelta(
+        days=sast_config.audit_timerange_days
+    )
 
     return delivery_client.greatest_component_versions(
-        component_name=cm06_config.component_name,
+        component_name=sast_config.component_name,
         start_date=end_date,
         end_date=start_date,
+    )
+
+
+def create_missing_linter_finding(
+    component_name: str,
+    component_version: str,
+    sub_type: dso.model.SastSubType,
+    time_now: datetime.datetime,
+) -> dso.model.ArtefactMetadata:
+    return dso.model.ArtefactMetadata(
+        artefact=dso.model.ComponentArtefactId(
+            component_name=component_name,
+            component_version=component_version,
+            artefact=dso.model.LocalArtefactId(
+                artefact_name=None,
+                artefact_type=None,
+            )
+        ),
+        meta=dso.model.Metadata(
+            datasource=dso.model.Datasource.SAST_LINT_CHECK,
+            type=dso.model.Datatype.SAST_FINDING,
+            creation_date=time_now,
+            last_update=time_now,
+        ),
+        data=dso.model.SastFinding(
+            sast_status=dso.model.SastStatus.NO_LINTER,
+            severity=github.compliance.model.Severity.BLOCKER,
+            sub_type=sub_type,
+        ),
+        discovery_date=time_now.date(),
     )
 
 
@@ -140,7 +155,7 @@ def parse_args():
 
     parser.add_argument(
         '--k8s-cfg-name',
-        help='specify kubernetes cluster to interact with',
+        help='kubernetes cluster to use',
         default=os.environ.get('K8S_CFG_NAME'),
     )
     parser.add_argument(
@@ -156,7 +171,7 @@ def parse_args():
         default=os.environ.get('K8S_TARGET_NAMESPACE'),
     )
     parser.add_argument(
-        '--cfg-name',
+        '--scan-cfg-name',
         help='''
             specify the context the process should run in, not relevant for the artefact
             enumerator as well as backlog controller as these are context independent
@@ -180,10 +195,10 @@ def parse_args():
             'or via environment variable "K8S_TARGET_NAMESPACE"'
         )
 
-    if not parsed_arguments.cfg_name:
+    if not parsed_arguments.scan_cfg_name:
         raise ValueError(
             'name of the to-be-used scan configuration must be set, either via '
-            'argument "--cfg-name" or via environment variable "CFG_NAME"'
+            'argument "--scan-cfg-name" or via environment variable "CFG_NAME"'
         )
 
     return parsed_arguments
@@ -191,7 +206,7 @@ def parse_args():
 
 def main():
     parsed_arguments = parse_args()
-    cfg_name = parsed_arguments.cfg_name
+    scan_cfg_name = parsed_arguments.scan_cfg_name
     namespace = parsed_arguments.k8s_namespace
     delivery_service_url = parsed_arguments.delivery_service_url
 
@@ -206,106 +221,115 @@ def main():
         )
 
     k8s.logging.init_logging_thread(
-        service=config.Services.CM06,
+        service=config.Services.SAST_LINT_CHECK,
         namespace=namespace,
         kubernetes_api=kubernetes_api,
-
     )
     atexit.register(
         k8s.logging.log_to_crd,
-        service=config.Services.CM06,
+        service=config.Services.SAST_LINT_CHECK,
         namespace=namespace,
         kubernetes_api=kubernetes_api,
     )
 
-    cm06_config = deserialise_cm06_configuration(
-        cfg_name=cfg_name,
+    sast_config = deserialise_sast_configuration(
+        scan_cfg_name=scan_cfg_name,
         namespace=namespace,
         kubernetes_api=kubernetes_api,
     )
 
     if not delivery_service_url:
-        delivery_service_url = cm06_config.delivery_service_url
+        delivery_service_url = sast_config.delivery_service_url
 
     delivery_client = delivery.client.DeliveryServiceClient(
         routes=delivery.client.DeliveryServiceRoutes(
-            base_url=parsed_arguments.delivery_service_url,
+            base_url=delivery_service_url,
         ),
         cfg_factory=cfg_factory,
     )
 
     component_descriptor_lookup = lookups.init_component_descriptor_lookup(
-        cache_dir=default_cache_dir,
+        cache_dir=parsed_arguments.cache_dir,
         delivery_client=delivery_client,
     )
 
-    landscape_versions = get_landscape_versions(
-        cm06_config=cm06_config,
+    component_versions = get_component_versions(
+        sast_config=sast_config,
         delivery_client=delivery_client,
     )
 
     new_metadata = []
     time_now = datetime.datetime.now(datetime.timezone.utc)
 
-    for component_version in sorted(landscape_versions, key=semver.VersionInfo.parse):
+    for component_version in sorted(component_versions, key=semver.VersionInfo.parse):
         component_descriptor = component_descriptor_lookup(
             ocm.ComponentIdentity(
-                name=cm06_config.component_name,
+                name=sast_config.component_name,
                 version=component_version,
             )
         )
 
-        component_nodes = list(
-            cnudie.iter.iter(
-                component=component_descriptor.component,
-                lookup=component_descriptor_lookup,
-                node_filter=cnudie.iter.Filter.components,
-                prune_unique=True,
+        for cnode in cnudie.iter.iter(
+            component=component_descriptor.component,
+            lookup=component_descriptor_lookup,
+            node_filter=cnudie.iter.Filter.components,
+            prune_unique=True,
+        ):
+            new_metadata.append(
+                dso.model.ArtefactMetadata(
+                    artefact=dso.model.ComponentArtefactId(
+                        component_name=cnode.component.name,
+                        component_version=cnode.component.version,
+                        artefact=dso.model.LocalArtefactId(
+                            artefact_name=None,
+                            artefact_type=None,
+                        )
+                    ),
+                    meta=dso.model.Metadata(
+                        datasource=dso.model.Datasource.SAST_LINT_CHECK,
+                        type=dso.model.Datatype.ARTEFACT_SCAN_INFO,
+                        creation_date=time_now,
+                        last_update=time_now,
+                    ),
+                    data=None,
+                    discovery_date=time_now.date(),
+                )
             )
-        )
 
-        for cnode in component_nodes:
-            sast_status_value = sast_status(
-                component=cnode.component
-            )
+            all_findings_for_rescoring = []
 
-            data_field = dso.model.SastFinding(
-                component_context=determine_component_context(
-                    component=cnode.component,
-                    cfg=cm06_config
-                ),
-                sast_statuses=sast_status_value.value,
-                severity=github.compliance.model.Severity.BLOCKER,
-            )
-
-            original_finding = dso.model.ArtefactMetadata(
-                artefact=dso.model.ComponentArtefactId(
+            if not has_local_linter(cnode.component):
+                no_linter_local = create_missing_linter_finding(
                     component_name=cnode.component.name,
                     component_version=cnode.component.version,
-                    artefact=dso.model.LocalArtefactId(
-                        artefact_name=None,
-                        artefact_type=None,
-                    )
-                ),
-                meta=dso.model.Metadata(
-                    datasource=dso.model.Datasource.CM06,
-                    type=dso.model.Datatype.SAST_FINDING,
-                    creation_date=time_now,
-                    last_update=time_now,
-                ),
-                data=data_field
-            )
-            new_metadata.append(original_finding)
-
-            for ruleset in cm06_config.sast_rescoring_rulesets:
-                rescored_metadata = rescore.utility.generate_sast_rescorings(
-                    findings=[original_finding],
-                    sast_rescoring_ruleset=ruleset,
-                    user=dso.model.User(
-                        username='sast-extension-auto-rescoring'
-                    ),
+                    sub_type=dso.model.SastSubType.LOCAL_LINTING,
+                    time_now=time_now,
                 )
-                new_metadata.extend(rescored_metadata)
+                new_metadata.append(no_linter_local)
+                all_findings_for_rescoring.append(no_linter_local)
+
+            if not is_central_linter_ignored(cnode.component):
+                no_linter_central = create_missing_linter_finding(
+                    component_name=cnode.component.name,
+                    component_version=cnode.component.version,
+                    sub_type=dso.model.SastSubType.CENTRAL_LINTING,
+                    time_now=time_now,
+                )
+                new_metadata.append(no_linter_central)
+                all_findings_for_rescoring.append(no_linter_central)
+
+            if not all_findings_for_rescoring or not sast_config.sast_rescoring_ruleset:
+                continue
+
+            rescored_metadata = rescore.utility.iter_sast_rescorings(
+                findings=all_findings_for_rescoring,
+                sast_rescoring_ruleset=sast_config.sast_rescoring_ruleset,
+                user=dso.model.User(
+                    username='sast-extension-auto-rescoring'
+                ),
+                time_now=time_now,
+            )
+            new_metadata.extend(rescored_metadata)
 
     delivery_client.update_metadata(data=new_metadata)
 
