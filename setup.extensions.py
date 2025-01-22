4b0d9376de38e6bc13e2e73884dd23250aa276d2
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2025-01-22 12:47:21 +0100

    Remove (now) unused `requirements.extensions` file

--- setup.extensions.py
+++ setup.extensions.py
@@ -11,14 +11,6 @@ own_dir = os.path.abspath(os.path.dirname(__file__))
 def requirements():
     yield 'ocm-gear-utils'
 
-    with open(os.path.join(own_dir, 'requirements.extensions.txt')) as f:
-        for line in f.readlines():
-            line = line.strip()
-            if not line or line.startswith('#'):
-                continue
-
-            yield line
-
 
 def modules():
     return [
