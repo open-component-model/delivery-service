15d7719d45c550d06726ed8711656f4c8fd0347c
TuanAnh17N <Tuan-Anh_Nguyen@gmx.de>
2025-02-05 17:32:50 +0100

    integrate artefact enumerator and backlog processing

--- sast.py
+++ sast.py
@@ -6,6 +6,9 @@ import datetime
 import enum
 import logging
 import os
+import signal
+import sys
+import time
 
 import ci.log
 import cnudie.iter
@@ -14,10 +17,10 @@ import delivery.client
 import dso.labels
 import dso.model
 import ocm
-import version
 
-import config
+import consts
 import ctx_util
+import k8s.backlog
 import k8s.util
 import k8s.model
 import k8s.logging
@@ -27,7 +30,6 @@ import odg.scan_cfg
 import paths
 import rescore.model
 import rescore.utility
-import util
 
 
 logger = logging.getLogger(__name__)
@@ -35,6 +37,22 @@ ci.log.configure_default_logging()
 
 own_dir = os.path.abspath(os.path.dirname(__file__))
 default_cache_dir = os.path.join(own_dir, '.cache')
+
+ready_to_terminate = True
+wants_to_terminate = False
+
+
+def handle_termination_signal(*args):
+    global ready_to_terminate, wants_to_terminate
+
+    # also terminate if > 1 termination signals were received
+    if ready_to_terminate or wants_to_terminate:
+        sys.exit(0)
+
+    # grace period to finish current scan is defined in the replica set
+    # after this period, the scan will be terminated anyways by k8s means
+    logger.info('termination signal received, will try to finish current scan and then exit')
+    wants_to_terminate = True
 
 
 class AnalysisLabel(enum.StrEnum):
@@ -72,23 +90,13 @@ def find_scan_policy(
 
 
 def create_missing_linter_finding(
-    snode: cnudie.iter.SourceNode,
+    artefact: dso.model.ComponentArtefactId,
     sub_type: dso.model.SastSubType,
     categorisation: odg.findings.FindingCategorisation,
     creation_timestamp: datetime.datetime=datetime.datetime.now(tz=datetime.timezone.utc),
 ) -> dso.model.ArtefactMetadata | None:
     return dso.model.ArtefactMetadata(
-        artefact=dso.model.ComponentArtefactId(
-            component_name=snode.component.name,
-            component_version=snode.component.version,
-            artefact=dso.model.LocalArtefactId(
-                artefact_name=snode.source.name,
-                artefact_type=snode.source.type,
-                artefact_version=snode.source.version,
-                artefact_extra_id=snode.source.extraIdentity,
-            ),
-            artefact_kind=dso.model.ArtefactKind.SOURCE,
-        ),
+        artefact=artefact,
         meta=dso.model.Metadata(
             datasource=dso.model.Datasource.SAST_LINT_CHECK,
             type=dso.model.Datatype.SAST_FINDING,
@@ -202,7 +210,7 @@ def fetch_component_versions(
 def iter_sast_artefacts_for_sub_type(
     sast_finding_config: odg.findings.Finding,
     sub_type: dso.model.SastSubType,
-    source_node: cnudie.iter.SourceNode,
+    artefact: dso.model.ComponentArtefactId,
     creation_timestamp: datetime.datetime=datetime.datetime.now(datetime.timezone.utc),
 ) -> collections.abc.Generator[dso.model.ArtefactMetadata, None, None]:
     categorisation = odg.findings.categorise_finding(
@@ -214,7 +222,7 @@ def iter_sast_artefacts_for_sub_type(
         return
 
     missing_linter_finding = create_missing_linter_finding(
-        snode=source_node,
+        artefact=artefact,
         sub_type=sub_type,
         categorisation=categorisation,
         creation_timestamp=creation_timestamp,
@@ -243,78 +251,70 @@ def iter_sast_artefacts_for_sub_type(
 
 
 def iter_artefact_metadata(
-    component_descriptor: ocm.ComponentDescriptor,
+    artefact: dso.model.ComponentArtefactId,
     component_descriptor_lookup: cnudie.retrieve.ComponentDescriptorLookupById,
     sast_finding_config: odg.findings.Finding,
-    creation_timestamp: datetime.datetime,
+    sast_config: odg.scan_cfg.SASTConfig,
+    creation_timestamp: datetime.datetime = datetime.datetime.now(datetime.timezone.utc),
 ) -> collections.abc.Generator[dso.model.ArtefactMetadata, None, None]:
     '''
     Processes source nodes for a given component descriptor, yielding SAST metadata.
     Handles resource filtering, local linter findings, and rescoring logic.
     '''
-    for snode in cnudie.iter.iter(
-        component=component_descriptor.component,
-        lookup=component_descriptor_lookup,
-        node_filter=cnudie.iter.Filter.sources,
-        prune_unique=True,
-    ):
-        artefact = dso.model.component_artefact_id_from_ocm(
-            component=snode.component,
-            artefact=snode.source,
+    if not sast_finding_config.matches(artefact):
+        logger.info(f'SAST findings are filtered out for {artefact=}, skipping...')
+        return
+
+    if not sast_config.is_supported(artefact_kind=artefact.artefact_kind):
+        if sast_config.on_unsupported is odg.scan_cfg.WarningVerbosities.FAIL:
+            raise TypeError(
+                f'{artefact.artefact_kind} is not supported by the SAST extension, maybe the filter '
+                'configurations have to be adjusted to filter out this artefact kind'
+            )
+        return
+
+    source_node = k8s.util.get_ocm_node(
+        component_descriptor_lookup=component_descriptor_lookup,
+        artefact=artefact,
+    )
+    if len(source_node.component.sources) == 1:
+        resources = source_node.component.resources
+    else:
+        resources = [
+            resource
+            for resource in source_node.component.resources
+            for src_ref in resource.srcRefs
+            # only support identity selector for now
+            if src_ref.identitySelector.get('name') == source_node.source.name
+        ]
+
+    if not has_local_linter(resources):
+        yield from iter_sast_artefacts_for_sub_type(
+            sast_finding_config=sast_finding_config,
+            sub_type=dso.model.SastSubType.LOCAL_LINTING,
+            artefact=artefact,
+            creation_timestamp=creation_timestamp,
         )
 
-        if not sast_finding_config.matches(artefact):
-            logger.info(f'SAST findings are filtered out for {artefact=}, skipping...')
-            continue
-
-        if len(snode.component.sources) == 1:
-            resources = snode.component.resources
-        else:
-            resources = [
-                resource
-                for resource in snode.component.resources
-                for src_ref in resource.srcRefs
-                # only support identity selector for now
-                if src_ref.identitySelector.get('name') == snode.source.name
-            ]
-
-        if not has_local_linter(resources):
-            yield from iter_sast_artefacts_for_sub_type(
-                sast_finding_config=sast_finding_config,
-                sub_type=dso.model.SastSubType.LOCAL_LINTING,
-                source_node=snode,
-                creation_timestamp=creation_timestamp,
-            )
-
-        if not find_scan_policy(snode) is dso.labels.ScanPolicy.SKIP:
-            yield from iter_sast_artefacts_for_sub_type(
-                sast_finding_config=sast_finding_config,
-                sub_type=dso.model.SastSubType.CENTRAL_LINTING,
-                source_node=snode,
-                creation_timestamp=creation_timestamp,
-            )
-
-        yield dso.model.ArtefactMetadata(
-            artefact=dso.model.ComponentArtefactId(
-                component_name=snode.component.name,
-                component_version=snode.component.version,
-                artefact=dso.model.LocalArtefactId(
-                    artefact_name=snode.source.name,
-                    artefact_type=snode.source.type,
-                    artefact_version=snode.source.version,
-                    artefact_extra_id=snode.source.extraIdentity,
-                ),
-                artefact_kind=dso.model.ArtefactKind.SOURCE,
-            ),
-            meta=dso.model.Metadata(
-                datasource=dso.model.Datasource.SAST_LINT_CHECK,
-                type=dso.model.Datatype.ARTEFACT_SCAN_INFO,
-                creation_date=creation_timestamp,
-                last_update=creation_timestamp,
-            ),
-            data={},
-            discovery_date=creation_timestamp.date(),
+    if not find_scan_policy(source_node) is dso.labels.ScanPolicy.SKIP:
+        yield from iter_sast_artefacts_for_sub_type(
+            sast_finding_config=sast_finding_config,
+            sub_type=dso.model.SastSubType.CENTRAL_LINTING,
+            artefact=artefact,
+            creation_timestamp=creation_timestamp,
         )
+
+    yield dso.model.ArtefactMetadata(
+        artefact=artefact,
+        meta=dso.model.Metadata(
+            datasource=dso.model.Datasource.SAST_LINT_CHECK,
+            type=dso.model.Datatype.ARTEFACT_SCAN_INFO,
+            creation_date=creation_timestamp,
+            last_update=creation_timestamp,
+        ),
+        data={},
+        discovery_date=creation_timestamp.date(),
+    )
 
 
 def parse_args():
@@ -366,6 +366,9 @@ def parse_args():
 
 
 def main():
+    signal.signal(signal.SIGTERM, handle_termination_signal)
+    signal.signal(signal.SIGINT, handle_termination_signal)
+
     parsed_arguments = parse_args()
     namespace = parsed_arguments.k8s_namespace
     delivery_service_url = parsed_arguments.delivery_service_url
@@ -373,7 +376,7 @@ def main():
     secret_factory = ctx_util.secret_factory()
 
     if parsed_arguments.k8s_cfg_name:
-        kubernetes_cfg = secret_factory.kubernetes_cfg(parsed_arguments.k8s_cfg_name)
+        kubernetes_cfg = secret_factory.kubernetes(parsed_arguments.k8s_cfg_name)
         kubernetes_api = k8s.util.kubernetes_api(kubernetes_cfg=kubernetes_cfg)
     else:
         kubernetes_api = k8s.util.kubernetes_api(
@@ -424,17 +427,49 @@ def main():
         cache_dir=parsed_arguments.cache_dir,
         delivery_client=delivery_client,
     )
-    version_lookup = lookups.init_version_lookup()
-    all_metadata = list(
-        iter_sast_metadata(
-            sast_config=sast_config,
-            sast_finding_config=sast_finding_config,
-            component_descriptor_lookup=component_descriptor_lookup,
-            version_lookup=version_lookup,
-        )
-    )
 
-    delivery_client.update_metadata(data=all_metadata)
+    global ready_to_terminate, wants_to_terminate
+    while not wants_to_terminate:
+        ready_to_terminate = False
+
+        backlog_crd = k8s.backlog.get_backlog_crd_and_claim(
+            service=odg.scan_cfg.Services.SAST_LINT_CHECK,
+            namespace=namespace,
+            kubernetes_api=kubernetes_api,
+        )
+
+        if not backlog_crd:
+            ready_to_terminate = True
+            sleep_interval_seconds = consts.BACKLOG_ITEM_SLEEP_INTERVAL_SECONDS
+            logger.info(f'no open backlog item found, will sleep for {sleep_interval_seconds=}')
+            time.sleep(sleep_interval_seconds)
+            continue
+
+        name = backlog_crd.get('metadata').get('name')
+        logger.info(f'processing backlog item {name}')
+
+        backlog_item = k8s.backlog.BacklogItem.from_dict(
+            backlog_item=backlog_crd.get('spec'),
+        )
+
+        all_metadata = list(
+            iter_artefact_metadata(
+                artefact=backlog_item.artefact,
+                component_descriptor_lookup=component_descriptor_lookup,
+                sast_finding_config=sast_finding_config,
+                sast_config=sast_config,
+            )
+        )
+
+        delivery_client.update_metadata(data=all_metadata)
+
+        k8s.util.delete_custom_resource(
+            crd=k8s.model.BacklogItemCrd,
+            name=name,
+            namespace=namespace,
+            kubernetes_api=kubernetes_api,
+        )
+        logger.info(f'processed and deleted backlog item {name}')
 
 
 if __name__ == '__main__':
