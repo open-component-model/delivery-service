8b7ffe8a341026f34dda9a2a4b030a4ae9d57651
Philipp Heil (zkdev) <dev@zeekay.dev>
2025-02-07 15:31:11 +0100

    Add build description

---
+++ setup.bdba.py
@@ -0,0 +1,31 @@
+import os
+
+import semver
+import setuptools
+
+
+own_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def requirements():
+    with open(os.path.join(own_dir, 'requirements.bdba.txt')) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line or line.startswith('#'):
+                continue
+
+            yield line
+
+
+def finalize_version():
+    with open(os.path.join(own_dir, 'BDBA_VERSION')) as f:
+        return semver.finalize_version(f.read().strip())
+
+
+setuptools.setup(
+    name='bdba',
+    version=finalize_version(),
+    py_modules=[],
+    packages=['bdba'],
+    install_requires=list(requirements()),
+)
