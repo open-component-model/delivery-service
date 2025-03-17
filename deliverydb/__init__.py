dc3f81a762e6f54e7077f1c771f17e05fb2352a3
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2025-03-17 15:06:34 +0100

    Make DB pool parameters configurable in sessionmaker

--- deliverydb/__init__.py
+++ deliverydb/__init__.py
@@ -11,15 +11,18 @@ def do_raise(self):
 # prevent usage of postgresql exclusive `JSONB`
 sap.JSONB.__init__ = do_raise
 
-sessionmaker = None
+sessionmakers: dict[tuple[int, int, int], sqlasync.async_sessionmaker[sqlasync.session.AsyncSession]] = {} # noqa: E501
 
 
 async def _sqlalchemy_sessionmaker(
     db_url: str,
+    pool_size: int=5,
+    max_overflow: int=10,
+    pool_timeout: int=30,
 ) -> sqlasync.async_sessionmaker[sqlasync.session.AsyncSession]:
-    # use singleton instead of caching to prevent issues with coroutines as return type
-    global sessionmaker
-    if sessionmaker:
+    # don't use regular caching here to prevent issues with coroutines as return type
+    global sessionmakers
+    if sessionmaker := sessionmakers.get((pool_size, max_overflow, pool_timeout)):
         return sessionmaker
 
     engine = sqlasync.create_async_engine(
@@ -27,22 +30,37 @@ async def _sqlalchemy_sessionmaker(
         echo=False,
         future=True,
         pool_pre_ping=True,
+        pool_size=pool_size,
+        max_overflow=max_overflow,
+        pool_timeout=pool_timeout,
     )
 
     async with engine.begin() as conn:
         await conn.run_sync(dm.Base.metadata.create_all)
 
     sessionmaker = sqlasync.async_sessionmaker(bind=engine)
+    sessionmakers[(pool_size, max_overflow, pool_timeout)] = sessionmaker
+
     return sessionmaker
 
 
-async def sqlalchemy_session(db_url: str) -> sqlasync.session.AsyncSession:
+async def sqlalchemy_session(
+    db_url: str,
+    pool_size: int=5,
+    max_overflow: int=10,
+    pool_timeout: int=30,
+) -> sqlasync.session.AsyncSession:
     '''
     Caller must close database-session.
 
     Using session object managed by `middleware.db_session_middleware` middleware is the preferred
     way to obtain a database-session.
     '''
-    sessionmaker = await _sqlalchemy_sessionmaker(db_url=db_url)
+    sessionmaker = await _sqlalchemy_sessionmaker(
+        db_url=db_url,
+        pool_size=pool_size,
+        max_overflow=max_overflow,
+        pool_timeout=pool_timeout,
+    )
 
     return sessionmaker()
