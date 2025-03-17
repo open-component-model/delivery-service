77f0ae8d66c43beb4b6a325002dd42d07453eabc
Aleksandar Savchev <57963548+AleksandarSavchev@users.noreply.github.com>
2025-03-17 09:02:50 +0200

    Improve diki rule description in diki issue template (#388)
    * Improve diki rule summary
    
    * Be more descriptive in rule desc
    
    * Fix lint errors
    
    * Add suggested changes
    
    * Add severity

--- issue_replicator/github.py
+++ issue_replicator/github.py
@@ -663,17 +663,36 @@ def _diki_template_vars(
     shortened_summary = summary
     for finding in findings:
 
-        finging_rule = finding.finding.data
+        finding_rule = finding.finding.data
         finding_str = '\n'
-        finding_str += f'# Failed {finging_rule.ruleset_id}:{finging_rule.ruleset_version}'
-        finding_str += f' rule with ID {finging_rule.rule_id} - {finging_rule.severity}\n'
+        finding_str += '# Failed rule summary\n'
+        finding_str += '|    |    |\n'
+        finding_str += '| -- | -- |\n'
+        finding_str += f'| Ruleset ID | {finding_rule.ruleset_id} |\n'
+        finding_str += f'| Ruleset Name | {finding_rule.ruleset_name} |\n'
+        finding_str += f'| Ruleset Version | {finding_rule.ruleset_version} |\n'
+        finding_str += f'| Rule ID | {finding_rule.rule_id} |\n'
+        finding_str += f'| Rule Name | {finding_rule.rule_name} |\n'
+        finding_str += f'| Severity | {finding_rule.severity} |\n'
+
+        rule_desc = ""
+        match finding_rule.ruleset_id:
+            case "disa-kubernetes-stig":
+                rule_desc = f'[DISA STIG viewer - {finding_rule.rule_id}](https://www.stigviewer.com/stig/kubernetes/2024-08-22/finding/V-{finding_rule.rule_id})'  # noqa: E501
+            case "security-hardened-shoot-cluster":
+                rule_desc = f'[Security Hardened Shoot Cluster Guide - {finding_rule.rule_id}](https://github.com/gardener/diki/blob/main/docs/rulesets/security-hardened-shoot-cluster/ruleset.md#{finding_rule.rule_id})' # noqa: E501
+            case "security-hardened-k8s":
+                rule_desc = f'[Security Hardened Kubernetes Cluster Guide - {finding_rule.rule_id}](https://github.com/gardener/diki/blob/main/docs/rulesets/security-hardened-k8s/ruleset.md#{finding_rule.rule_id})' # noqa: E501
+
+        if len(rule_desc) > 0:
+            finding_str += f'| Rule Description | {rule_desc} |\n'
         finding_str += '\n'
         finding_str += '### Failed checks:\n'
 
         summary += finding_str
         shortened_summary += finding_str
 
-        for check in finging_rule.checks:
+        for check in finding_rule.checks:
             check_msg_str = '\n'
             check_msg_str += f'Message: {check.message}\n'
             check_msg_str += 'Targets:\n'
