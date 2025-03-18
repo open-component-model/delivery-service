8f319d651facdbdb7f6807bf0c2d37e188c9184a
TuanAnh17N <Tuan-Anh_Nguyen@gmx.de>
2025-03-18 10:44:09 +0100

    Extend rescoring proposals

--- rescore/artefacts.py
+++ rescore/artefacts.py
@@ -67,6 +67,7 @@ class RescoringProposal:
         | MalwareFinding
         | dso.model.SastFinding
         | dso.model.CryptoFinding
+        | dso.model.OsIdFinding
     )
     finding_type: str
     severity: str
@@ -490,6 +491,19 @@ async def _iter_rescoring_proposals(
                 config=dacite.Config(
                     strict=True,
                 ),
+            )
+        elif finding_cfg.type is odg.findings.FindingType.OS_ID:
+            yield dacite.from_dict(
+                data_class=RescoringProposal,
+                data={
+                    'finding': dataclasses.asdict(am.data),
+                    'finding_type': finding_cfg.type,
+                    'severity': current_severity,
+                    'matching_rules': matching_rule_names,
+                    'applicable_rescorings': serialised_current_rescorings,
+                    'discovery_date': am.discovery_date.isoformat(),
+                    'sprint': sprint,
+                },
             )
 
         seen_ids.add(am.id)
