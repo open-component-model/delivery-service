4bd00b6315bd7101be7139015aad149587d89c0d
Aleksandar Savchev <aleksandar.savchev@sap.com>
2025-04-22 16:26:08 +0300

    Add suggested change

--- issue_replicator/github.py
+++ issue_replicator/github.py
@@ -680,25 +680,19 @@ def _diki_template_vars(
             case "disa-kubernetes-stig":
                 rule_desc = f'[DISA STIG viewer - {finding_rule.rule_id}](https://www.stigviewer.com/stig/kubernetes/2024-08-22/finding/V-{finding_rule.rule_id})'  # noqa: E501
             case "security-hardened-shoot-cluster":
-                diki_version = ""
-
-                match finding_rule.ruleset_version:
-                    case "v0.1.0":
-                        diki_version = "v0.14.0"
-                    case "v0.2.0":
-                        diki_version = "v0.15.0"
-                    case _:
-                        diki_version = "main"
+                diki_version_for_ruleset_version = {
+                    'v0.1.0': 'v0.14.0',
+                    'v0.2.0': 'v0.15.0',
+                    'v0.2.1': 'v0.15.1',
+                }
+                diki_version = diki_version_for_ruleset_version.get(finding_rule.ruleset_version, 'main')
 
                 rule_desc = f'[Security Hardened Shoot Cluster Guide - {finding_rule.rule_id}](https://github.com/gardener/diki/blob/{diki_version}/docs/rulesets/security-hardened-shoot-cluster/ruleset.md#{finding_rule.rule_id})' # noqa: E501
             case "security-hardened-k8s":
-                diki_version = ""
-
-                match finding_rule.ruleset_version:
-                    case "v0.1.0":
-                        diki_version = "v0.15.0"
-                    case _:
-                        diki_version = "main"
+                diki_version_for_ruleset_version = {
+                    'v0.1.0': 'v0.15.0',
+                }
+                diki_version = diki_version_for_ruleset_version.get(finding_rule.ruleset_version, 'main')
 
                 rule_desc = f'[Security Hardened Kubernetes Cluster Guide - {finding_rule.rule_id}](https://github.com/gardener/diki/blob/{diki_version}/docs/rulesets/security-hardened-k8s/ruleset.md#{finding_rule.rule_id})' # noqa: E501
         if len(rule_desc) > 0:
