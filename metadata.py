5fc2d010c00974c0eda6305f1785b1c66c3e85a8
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2025-05-16 12:55:43 +0200

    fix: Check if new `meta` attributes exist (backwards-compatibility)

--- metadata.py
+++ metadata.py
@@ -409,13 +409,15 @@ class ArtefactMetadata(aiohttp.web.View):
 
                 # create new dict instead of patching it, otherwise it won't be updated in the db
                 del existing_entry.meta['last_update']
-                del existing_entry.meta['responsibles']
-                del existing_entry.meta['assignee_mode']
+                if 'responsibles' in existing_entry.meta:
+                    del existing_entry.meta['responsibles']
+                if 'assignee_mode' in existing_entry.meta:
+                    del existing_entry.meta['assignee_mode']
                 existing_entry.meta = dict(
                     **existing_entry.meta,
                     last_update=metadata_entry.meta['last_update'],
-                    responsibles=metadata_entry.meta['responsibles'],
-                    assignee_mode=metadata_entry.meta['assignee_mode'],
+                    responsibles=metadata_entry.meta.get('responsibles'),
+                    assignee_mode=metadata_entry.meta.get('assignee_mode'),
                 )
 
             await db_session.commit()
