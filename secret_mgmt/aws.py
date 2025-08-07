815ef0a10e53f0db5d17ad3517284aaf58378178
Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>
2025-08-07 11:12:03 +0200

    Only require `aws_secret_name` if more than one AWS secret is known
    Signed-off-by: Jonas Brand (8R0WNI3) <jonasbrand1712@gmail.com>

--- secret_mgmt/aws.py
+++ secret_mgmt/aws.py
@@ -3,6 +3,8 @@ import functools
 
 import boto3
 
+import secret_mgmt
+
 
 @dataclasses.dataclass
 class AWS:
@@ -17,3 +19,21 @@ class AWS:
             aws_secret_access_key=self.secret_access_key,
             region_name=self.region,
         )
+
+
+def find_cfg(
+    secret_factory: secret_mgmt.SecretFactory,
+    secret_name: str | None=None,
+) -> AWS | None:
+    if secret_name:
+        return secret_factory.aws(secret_name)
+
+    aws_cfgs: list[AWS] = secret_factory.aws()
+
+    if len(aws_cfgs) == 1:
+        return aws_cfgs[0]
+
+    raise ValueError(
+        'AWS secret name must be specified if more than one secret is available (found '
+        f'{len(aws_cfgs)})'
+    )
