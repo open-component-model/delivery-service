3e4440c34a7b5763605a3144a972095e9f749061
I557814 <franziska.schallhorn@sap.com>
2025-12-16 09:06:44 +0100

    add changes according to PR comments

--- test/test_odg_extension_test_results.py
+++ test/test_odg_extension_test_results.py
@@ -5,174 +5,57 @@ import ocm
 import odg.model
 import odg.findings
 import test_results
+import yaml
+from pathlib import Path
 
 
 @pytest.fixture
 def component_descriptor() -> ocm.ComponentDescriptor:
-    raw = {
-        'component': {
-            'componentReferences': [],
-            'creationTime': '2025-10-07T09:15:29Z',
-            'labels': [],
-            'name': 'this/is/my/component',
-            'provider': 'Any',
-            'repositoryContexts': [
-                {
-                    'baseUrl': 'foo',
-                    'subPath': None,
-                    'type': 'ociRegistry'
-                }
-            ],
-            'resources': [
-                {
-                    'access': {
-                        'localReference': 'foo',
-                        'mediaType': 'application/data',
-                        'referenceName': None,
-                        'type': 'localBlob/v1'
-                    },
-                    'labels': [
-                        {
-                            'name': 'gardener.cloud/test-policy',
-                            'value': False
-                        },
-                        {
-                            'name': 'gardener.cloud/purposes',
-                            'signing': False,
-                            'value': ['test'],
-                            'version': None
-                        },
-                        {
-                            'name': 'gardener.cloud/test-scope',
-                            'value': ['job-image-1', 'job-image-2', 'job-image-4']
-                        }
-                    ],
-                    'name': 'job-image-test-1',
-                    'relation': 'local',
-                    'type': 'application/gzip',
-                    'version': '1.2710.0'
-                },
-                {
-                    'access': {
-                        'localReference': 'bar',
-                        'mediaType': 'application/data',
-                        'referenceName': None,
-                        'type': 'localBlob/v1'
-                    },
-                    'labels': [
-                        {
-                            'name': 'gardener.cloud/test-policy',
-                            'value': False
-                        },
-                        {
-                            'name': 'gardener.cloud/purposes',
-                            'signing': False,
-                            'value': ['test'],
-                            'version': None
-                        },
-                        {
-                            'name': 'gardener.cloud/test-scope',
-                            'value': ['job-image-2']
-                        }
-                    ],
-                    'name': 'job-image-test-2',
-                    'relation': 'local',
-                    'type': 'application/gzip',
-                    'version': '1.2710.0'
-                },
-                {
-                    'access': {
-                        'imageReference': 'foo',
-                        'type': 'ociRegistry'
-                    },
-                    'labels': [
-                        {
-                         'name': 'gardener.cloud/test-policy',
-                         'value': True
-                        }
-                    ],
-                    'name': 'job-image-1',
-                    'relation': 'local',
-                    'version': '1.2710.0',
-                    'type': 'ociImage'
-                },
-                {
-                    'access': {
-                        'imageReference': 'bar',
-                        'type': 'ociRegistry'
-                    },
-                    'labels': [
-                        {
-                         'name': 'gardener.cloud/test-policy',
-                         'value': True
-                        }
-                    ],
-                    'name': 'job-image-2',
-                    'relation': 'local',
-                    'version': '1.2710.0',
-                    'type': 'ociImage'
-                },
-                {
-                    'access': {
-                        'imageReference': 'bar',
-                        'type': 'ociRegistry'
-                    },
-                    'labels': [
-                        {
-                         'name': 'gardener.cloud/test-policy',
-                         'value': False
-                        }
-                    ],
-                    'name': 'job-image-3',
-                    'relation': 'external',
-                    'version': '1.2710.0',
-                    'type': 'helmChart/v1'
-                },
-            ],
-            'sources': [],
-            'version': '1.2710.0'
-        },
-    'meta': {
-        'schemaVersion': 'v2',
-    }
-}
+    yaml_path = Path(__file__).parent / "resources" / \
+                     "missing_test_results_component_descriptor.yaml"
+
+    raw = yaml.safe_load(yaml_path.read_text())
+
     return dacite.from_dict(
         data=raw,
         data_class=ocm.ComponentDescriptor,
-        config=dacite.Config(
-            cast=[enum.Enum]
-        )
+        config=dacite.Config(cast=[enum.Enum])
     )
 
 
 @pytest.fixture
 def missing_test_result_finding_cfg() -> odg.findings.Finding:
-    selector = odg.findings.TestResultFindingSelector(status=['.*'])
+    yaml_path_finding = Path(__file__).parent / \
+                             "resources" / "missing_test_result_finding.yaml"
 
-    categorisations = [
-        odg.findings.FindingCategorisation(
-            id='NONE',
-            display_name='test exists and has no findings',
-            value=0,
-            allowed_processing_time=0,
-            rescoring=None,
-            selector=None,
-        ),
-        odg.findings.FindingCategorisation(
-            id='BLOCKER',
-            display_name='Test result is missing',
-            value=16,
-            allowed_processing_time=0,
-            rescoring=None,
-            selector=selector,
-        ),
-    ]
+    raw = yaml.safe_load(yaml_path_finding.read_text())
+
+    categorisations = []
+
+    for cat in raw["categorisations"]:
+        selector = None
+
+        if cat["selector"] is not None:
+            selector = odg.findings.TestResultFindingSelector(
+                status=cat["selector"]["status"]
+            )
+
+        categorisations.append(
+            odg.findings.FindingCategorisation(
+                id=cat["id"],
+                display_name=cat["display_name"],
+                value=cat["value"],
+                allowed_processing_time=cat["allowed_processing_time"],
+                rescoring=cat["rescoring"],
+                selector=selector
+            )
+        )
 
     return odg.findings.Finding(
         type=odg.model.Datatype.TEST_RESULT_FINDING,
-        categorisations=categorisations,
         filter=None,
-        rescoring_ruleset=None
+        rescoring_ruleset=None,
+        categorisations=categorisations
     )
 
 
@@ -181,11 +64,11 @@ def test_artefact_test_results_filter(
     missing_test_result_finding_cfg: odg.findings.Finding
 ):
 
-    resources_req_tests = test_results.iter_artefacts_requiring_tests(
-        component=component_descriptor)
+    resources_req_tests = list(test_results.iter_artefacts_requiring_tests(
+        component=component_descriptor))
 
-    test_resources = test_results.find_test_artefacts(
-        component=component_descriptor)
+    test_resources = list(test_results.find_test_artefacts(
+        component=component_descriptor))
 
     assert len(resources_req_tests) == len(test_resources)
 
@@ -222,7 +105,7 @@ def test_artefact_test_results_filter(
         test_result_finding_config=missing_test_result_finding_cfg,
         sub_type=odg.model.TestStatus.NO_TEST)
 
-    assert test_coverage == None
+    assert len(test_coverage) == 0
 
 # let's remove the test results
     component_descriptor.component.resources = [
@@ -231,7 +114,7 @@ def test_artefact_test_results_filter(
         if not resource.name == 'job-image-test-2' and not resource.name == 'job-image-test-1'
     ]
 
-    test_resources = test_results.find_test_artefacts(
-        component=component_descriptor)
+    test_resources = list(test_results.find_test_artefacts(
+        component=component_descriptor))
 
     assert len(resources_req_tests) != len(test_resources)
