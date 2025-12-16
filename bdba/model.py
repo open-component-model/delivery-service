33af88cda016d699928b66d2e23cdf10c172b2f5
ben-yil <bedirhan.yilmaz@sap.com>
2025-12-16 15:40:11 +0100

    feat(bdba): Introduce SBoM format Enum

--- bdba/model.py
+++ bdba/model.py
@@ -273,3 +273,8 @@ class UploadStatus(enum.IntEnum):
     SKIPPED = 1
     PENDING = 2
     DONE = 4
+
+class SBomFormats(enum.StrEnum):
+    CYCLONEDX = 'cyclonedx'
+    SPDX = 'spdx'
+    BDIO = 'bdio'
