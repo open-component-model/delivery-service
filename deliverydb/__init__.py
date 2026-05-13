3f1bdd332cffb76bdc1d820295ebaeb272e8b2bb
Jonas Brand (8R0WNI3) <j.brand@sap.com>
2026-05-13 13:26:23 +0200

    Add synchronous database client (again)
    Signed-off-by: Jonas Brand (8R0WNI3) <j.brand@sap.com>

--- deliverydb/__init__.py
+++ deliverydb/__init__.py
@@ -1,5 +1,7 @@
+import sqlalchemy
 import sqlalchemy.dialects.postgresql as sap
 import sqlalchemy.ext.asyncio as sqlasync
+import sqlalchemy.orm.session
 
 import deliverydb.model as dm
 
@@ -14,6 +16,11 @@ sap.JSONB.__init__ = do_raise
 sessionmakers_async: dict[
     tuple[int, int, int],
     sqlasync.async_sessionmaker[sqlasync.session.AsyncSession],
+] = {}
+
+sessionmakers: dict[
+    tuple[int, int, int],
+    sqlalchemy.orm.sessionmaker[sqlalchemy.orm.session.Session],
 ] = {}
 
 
@@ -59,6 +66,49 @@ async def sqlalchemy_session_async(
     way to obtain a database-session.
     """
     sessionmaker = await _sqlalchemy_sessionmaker_async(
+        db_url=db_url,
+        pool_size=pool_size,
+        max_overflow=max_overflow,
+        pool_timeout=pool_timeout,
+    )
+
+    return sessionmaker()
+
+
+def _sqlalchemy_sessionmaker(
+    db_url: str,
+    pool_size: int = 5,
+    max_overflow: int = 10,
+    pool_timeout: int = 30,
+) -> sqlalchemy.orm.sessionmaker[sqlalchemy.orm.session.Session]:
+    if sessionmaker := sessionmakers.get((pool_size, max_overflow, pool_timeout)):
+        return sessionmaker
+
+    engine = sqlalchemy.create_engine(
+        db_url,
+        echo=False,
+        future=True,
+        pool_pre_ping=True,
+        pool_size=pool_size,
+        max_overflow=max_overflow,
+        pool_timeout=pool_timeout,
+    )
+
+    dm.Base.metadata.create_all(engine)
+
+    return sqlalchemy.orm.sessionmaker(bind=engine)
+
+
+def sqlalchemy_session(
+    db_url: str,
+    pool_size: int = 5,
+    max_overflow: int = 10,
+    pool_timeout: int = 30,
+) -> sqlalchemy.orm.session.Session:
+    '''
+    Caller must close database-session.
+    '''
+    sessionmaker = _sqlalchemy_sessionmaker(
         db_url=db_url,
         pool_size=pool_size,
         max_overflow=max_overflow,
